e12b177ecb31f6d028218fb92f3bfe1b38f18ef5
manpat <pat@patsown.com>
2014-11-09 18:23:07 +1000

    test level

---
+++ Assets/TEMPPLAYERSCRIPT.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TEMPPLAYERSCRIPT : MonoBehaviour {
+	public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vel = Vector3.zero;
+
+		vel.x = Input.GetAxis("Horizontal") * speed;
+		vel.y = Input.GetAxis("Vertical") * speed;
+
+		rigidbody2D.velocity = vel;
+	}
+}
