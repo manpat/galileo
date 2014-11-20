01df871821eb7632ad15a332d1495e7a4e2338ae
manpat <pat@patsown.com>
2014-11-21 09:44:45 +1000

    Restarted project, added test level, plaeholder player script

--- Assets/TEMPPLAYERSCRIPT.cs
+++ Assets/TEMPPLAYERSCRIPT.cs
@@ -4,18 +4,18 @@ using System.Collections;
 public class TEMPPLAYERSCRIPT : MonoBehaviour {
 	public float speed = 5f;
 
-	// Use this for initialization
+	private Rigidbody rb;
+
 	void Start () {
-	
+		rb = rigidbody;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		Vector3 vel = Vector3.zero;
+		Vector3 vel = rb.velocity;
 
 		vel.x = Input.GetAxis("Horizontal") * speed;
-		vel.y = Input.GetAxis("Vertical") * speed;
+		vel.z = Input.GetAxis("Vertical") * speed;
 
-		rigidbody2D.velocity = vel;
+		rb.velocity = vel;
 	}
 }
