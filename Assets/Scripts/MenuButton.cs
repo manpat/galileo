e58637cac8a85d7c46e1dc63d5a4dcf31bc0b16b
manpat <pat@patsown.com>
2014-12-01 22:00:38 +1000

    Added menu and basic functionality

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ButtonState {
+	Idle,
+	Hover,
+	Down,
+};
+
+public enum ButtonType {
+	None,
+	Start,
+	Quit,
+};
+
+public class MenuButton : MonoBehaviour {
+	public ButtonType type;
+
+	public float hoverScale = 0.1f;
+	public float downScale = -0.1f;
+	public float scaleRate = 0.1f; // % per s
+
+	public ButtonState state = ButtonState.Idle;
+	float startScale;
+	float currentScale;
+
+	// Use this for initialization
+	void Start () {
+		startScale = transform.localScale.x;
+		currentScale = startScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float targetScale = startScale;
+		switch(state){
+			case ButtonState.Hover:
+				targetScale *= (1f + hoverScale);
+				break;
+			case ButtonState.Down:
+				targetScale *= (1f + downScale);
+				break;
+		}
+
+		currentScale = targetScale; //Mathf.Lerp(currentScale, targetScale, Time.deltaTime * scaleRate);
+		transform.localScale = Vector3.one * currentScale;
+	}
+
+	void OnMouseOver(){
+		if(state != ButtonState.Down)
+			state = ButtonState.Hover;
+	}
+
+	void OnMouseExit(){
+		state = ButtonState.Idle;
+	}
+
+	void OnMouseDown(){
+		state = ButtonState.Down;
+	}
+	void OnMouseUpAsButton(){
+		state = ButtonState.Hover;
+		MenuSystem.OnButton(type);
+	}
+}
---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSystem : MonoBehaviour {
+	// static public MenuSystem main;
+
+	// void Awake(){
+	// 	main = this;
+	// }
+
+	static public void OnButton(ButtonType type){
+		switch(type){
+			case ButtonType.Start:
+				Application.LoadLevel("level1");
+				break;
+
+			case ButtonType.Quit:
+				Application.Quit();
+				break;
+
+			default:
+				Debug.LogError("OnButton " + type.ToString() + " not implemented");
+				break;
+		}
+	}
+}
