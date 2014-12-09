458734baeb8d1d360dfbd8fda96c761ba3667ee4
manpat <pat@patsown.com>
2014-12-09 15:15:28 +1000

    Merged animations and modified movement code to detect pushing

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class EventManager : MonoBehaviour {
 	static public EventManager main = null;
+
+	public WaypointThing fallingPlatform;
+	public float platformMoveSpeed;
+
+	private float platformMoveTimer = 0f;
+	private bool isCompoundCollected = false;
 
 	void Awake(){
 		if(main != null){
@@ -13,14 +20,32 @@ public class EventManager : MonoBehaviour {
 		main = this;
 	}
 
+	void Update(){
+		if(isCompoundCollected){
+			platformMoveTimer += Time.deltaTime;
+			Vector3 p0 = fallingPlatform.savedPositions[0];
+			Vector3 p1 = fallingPlatform.savedPositions[1];
+			float d = (p1 - p0).magnitude;
+
+			fallingPlatform.transform.position = Interp(p0, p1, platformMoveTimer/d);
+		}
+	}
+
 	// Changes the layout of the level as necessary. 
 	public void OnCompoundCollect(){
-
+		isCompoundCollected = true;
+		platformMoveTimer = -2f;
 	}
 
 	// Assumes the player has the compound. 
 	// Will trigger a cutscene or transition and load either the next level or the credits.
 	public void OnReturnToShip(){
 
+	}
+
+	private Vector3 Interp(Vector3 a, Vector3 b, float x){
+		x = Mathf.Clamp01(x);
+		x = x*x*x;
+		return Vector3.Lerp(a, b, x);
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,27 +11,32 @@ public class Player : MonoBehaviour {
 	public float vignetteShadeSensitivity = 0.7f;
 
 	public bool hasCollectedCompound = false;
+	public bool isPushing = false;
 
 	public Transform sun;
-	public Renderer ren;
 
 	private Transform tr;
 	private Rigidbody rb;
+	private Animator  an;
 
 	private float shadeAmount;
 
 	void Awake () {
 		tr = transform;
 		rb = rigidbody;
+		an = GetComponent<Animator>();
 	}
 	
 	void Update () {
 		CalcShadeAmount();
 
 		Vector3 vel = rb.velocity;
+		float s = isPushing?speedPushing:speed;
 
-		vel.x = Input.GetAxis("Horizontal") * speed;
-		vel.z = Input.GetAxis("Vertical") * speed;
+		vel.x = Input.GetAxis("Horizontal") * s;
+		vel.z = Input.GetAxis("Vertical") * s;
+
+		DoAnimations(vel/s);
 
 		rb.velocity = vel;
 		EffectsManager.main.SetDarkVignetteAlpha((1f - GetShadeAmount()) * vignetteShadeSensitivity);
@@ -65,6 +70,106 @@ public class Player : MonoBehaviour {
 			hasCollectedCompound = true;
 		}else if(col.gameObject.CompareTag("Ship") && hasCollectedCompound){
 			EventManager.main.OnReturnToShip();
+		}else if(col.gameObject.CompareTag("Pushable")){
+			isPushing = true;
+		}
+	}
+
+	void OnCollisionStay(Collision col){
+		if(col.gameObject.CompareTag("NotPushableAnyMore")){
+			isPushing = false;
+		}
+	}
+
+	void OnCollisionExit(Collision col){
+		if(col.gameObject.CompareTag("NotPushableAnyMore")
+			|| col.gameObject.CompareTag("Pushable")){
+			isPushing = false;
+		}
+	}
+
+	/*
+	* idle 		0
+	*
+	* walk up	1
+	* walk down	2
+	* walk left	3
+	* walk righ	4
+	*
+	* push up	5
+	* push down	6
+	* walk left	7
+	* walk righ	8
+	*/
+
+	private int animNum = -1;
+
+	void DoAnimations(Vector3 vel){
+		bool dirty = false;
+		if(vel.magnitude == 0f && animNum != 0){
+			animNum = 0;
+			dirty = true;
+
+		}else{
+			if(Mathf.Abs(vel.z) > Mathf.Abs(vel.x)){
+				if(vel.z > 0f && animNum != 1){
+					animNum = 1;
+					dirty = true;
+				}else if(vel.z < 0f && animNum != 2){
+					animNum = 2;
+					dirty = true;
+				}
+			}else{
+				if(vel.x > 0f && animNum != 3){
+					animNum = 3;
+					dirty = true;
+				}else if(vel.x < 0f && animNum != 4){
+					animNum = 4;
+					dirty = true;
+				}
+			}
+
+			if(isPushing && animNum < 5){
+				animNum += 4;
+				dirty = true;
+			}
+		}
+
+		if(dirty){
+			tr.localScale = new Vector3(1,1,1);
+
+			switch(animNum){
+				case 0:
+					an.Play("idle");
+					break;
+				case 1:
+					an.Play("walk_up");
+					break;
+				case 2:
+					an.Play("walk_down");
+					break;
+				case 3:
+					an.Play("walk_horizontal");
+					break;
+				case 4:
+					an.Play("walk_horizontal");
+					tr.localScale = new Vector3(-1,1,1);
+					break;
+
+				case 5:
+					an.Play("push_up");
+					break;
+				case 6:
+					an.Play("push_down");
+					break;
+				case 7:
+					an.Play("push_horizontal");
+					break;
+				case 8:
+					an.Play("push_horizontal");
+					tr.localScale = new Vector3(-1,1,1);
+					break;
+			}
 		}
 	}
 }
